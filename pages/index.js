57bfcf74da58f16877c42056480f4306692102ea
mertJF <mertcukuren@jotform.com>
2021-01-05 02:36:40 +0300

    temporary homepage

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,22 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="bg-gray-800 text-white h-screen overflow-hidden grid place-items-center content-center text-left">
       <Head>
-        <title>Create Next App</title>
+        <title>Mert Cukuren — UI Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <div className="flex flex-col">
+        <h1 className="text-4xl">Mert Cukuren</h1>
+        <h2 className="mt-2 text-gray-400">UI Developer at JotForm</h2>
+        <div className="flex space-x-4 mt-8">
+          <a href="https://twitter.com/knyttneve" className="text-gray-400 border-b-2 border-gray-700 text-sm hover:text-white" target="_blank">Twitter</a>
+          <a href="https://linkedin.com/in/mertcukuren" className="text-gray-400 border-b-2 border-gray-700 text-sm hover:text-white" target="_blank">Linkedin</a>
+          <a href="https://github.com/mertJF" className="text-gray-400 border-b-2 border-gray-700 text-sm hover:text-white" target="_blank">GitHub</a>
+          <a href="https://codepen.io/knyttneve" className="text-gray-400 border-b-2 border-gray-700 text-sm hover:text-white" target="_blank">CodePen</a>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
     </div>
   )
 }
