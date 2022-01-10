d9ce89a239a2d19be8134ebadc8c1c76b88097ea
Mert Cukuren <mertcukuren@gmail.com>
2022-01-10 18:05:59 +0300

    UI Developer -> UI Architect

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="bg-gray-800 text-white h-screen overflow-hidden grid place-items-center content-center text-left">
+    <div className="bg-gray-900 text-white h-screen overflow-hidden grid place-items-center content-center text-left">
       <Head>
-        <title>Mert Cukuren — UI Developer</title>
+        <title>Mert Cukuren — UI Architect</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex flex-col">
         <h1 className="text-4xl">Mert Cukuren</h1>
-        <h2 className="mt-2 text-gray-400">UI Developer at JotForm</h2>
+        <h2 className="mt-2 text-green-400">UI Architect at JotForm</h2>
         <div className="flex space-x-4 mt-8">
           <a href="https://twitter.com/knyttneve" className="text-gray-400 border-b-2 border-gray-700 text-sm hover:text-white" target="_blank">Twitter</a>
           <a href="https://linkedin.com/in/mertcukuren" className="text-gray-400 border-b-2 border-gray-700 text-sm hover:text-white" target="_blank">Linkedin</a>
