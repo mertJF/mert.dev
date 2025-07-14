de83f4177b49a340c417e170a3c2a890394c3695
mertJF <mertcukuren@jotform.com>
2025-07-14 11:11:55 +0300

    2025 edition v2

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,20 @@ import { Novatrix } from 'uvcanvas';
 export default function Home() {
 
   return (
-    <div className="bg-gray-800 h-screen p-20 overflow-hidden app" style={{ filter: 'sepia(1) hue-rotate(2000deg)' }}>
+    <div className="bg-gray-800 h-screen p-20 overflow-hidden app font-lexend tracking-xxs" style={{ filter: 'sepia(1) hue-rotate(2000deg)' }}>
       <Head>
         <title>Mert Cukuren — UI Architect</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Novatrix className="w-full h-full inset-0 h-full w-full absolute" style={{ filter: 'grayscale(1) brightness(0.6) contrast(1.8)' }} />
-      <div className="flex flex-col from-white to-transparent bg-gradient-to-r bg-opacity-90 absolute inset-0 sm:p-20 top-10 left-10 p-10" style={{ backdropFilter: 'blur(20px)' }}>
-        <h1 className="sm:text-5xl text-4xl text-gray-800">Mert Cukuren</h1>
-        <h2 className="mt-2 text-gray-700 sm:text-lg">UI Architect at Jotform</h2>
-        <div className="flex space-x-4 mt-8">
-          <a href="https://twitter.com/knyttneve" className="text-gray-700 border-b border-gray-700 text-sm hover:text-gray-800 hover:border-gray-800" target="_blank">Twitter</a>
-          <a href="https://linkedin.com/in/mertcukuren" className="text-gray-700 border-b border-gray-700 text-sm hover:text-gray-800 hover:border-gray-800" target="_blank">Linkedin</a>
-          <a href="https://github.com/mertJF" className="text-gray-700 border-b border-gray-700 text-sm hover:text-gray-800 hover:border-gray-800" target="_blank">GitHub</a>
-          <a href="https://codepen.io/knyttneve" className="text-gray-700 border-b border-gray-700 text-sm hover:text-gray-800 hover:border-gray-800" target="_blank">CodePen</a>
+      <Novatrix className="w-full h-full inset-0 h-full w-full absolute opacity-20" style={{ filter: 'grayscale(1) brightness(0.6) contrast(1.8)' }} />
+      <div className="flex flex-col bg-white bg-opacity-100 absolute sm:inset-20 inset-10 sm:p-20 top-10 left-10 p-10 rounded" style={{ boxShadow: '0 0 100px #ffffffbd' }}>
+        <h1 className="sm:text-7xl text-6xl text-gray-800">Mert Cukuren</h1>
+        <h2 className="mt-2 text-gray-700 sm:text-3xl text-2xl">UI Architect at <a href="https://jotform.com" target="_blank">Jotform</a></h2>
+        <div className="flex space-x-4 mt-8 sm:text-lg">
+          <a href="https://twitter.com/knyttneve" className="text-gray-700 border-b-2 border-gray-700 hover:text-gray-800 hover:border-gray-800" target="_blank">Twitter</a>
+          <a href="https://linkedin.com/in/mertcukuren" className="text-gray-700 border-b-2 border-gray-700 hover:text-gray-800 hover:border-gray-800" target="_blank">Linkedin</a>
+          <a href="https://github.com/mertJF" className="text-gray-700 border-b-2 border-gray-700 hover:text-gray-800 hover:border-gray-800" target="_blank">GitHub</a>
+          <a href="https://codepen.io/knyttneve" className="text-gray-700 border-b-2 border-gray-700 hover:text-gray-800 hover:border-gray-800" target="_blank">CodePen</a>
         </div>
       </div>
     </div>
